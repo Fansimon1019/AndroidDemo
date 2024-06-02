b73f944bd8da1cba320e2cc609ff9019fd1297b2
SIMON\范峻川 <junchuan1019.fan@gmail.com>
2024-06-02 09:45:42 +0800

    Update comment

--- app/src/main/java/com/simon/androiddemo/MainActivity.java
+++ app/src/main/java/com/simon/androiddemo/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //I am happy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
